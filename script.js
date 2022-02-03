c34ab83a18f3504e504fb8037b8882777b829da1
Andrei Btc <andreibtc@gmail.com>
2022-02-03 14:14:12 +0000

    Ads readme.md and functions descriptions

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // == FUNCTIONS ==
+
 // Greeter function
+/* Write a function which greets the user. 
+The function should receive one argument, a string. We can assume the string is a person’s name.
+We’d like the function to return "Hello " and then the name. 
+We should be able to see the output in the console. 
+*/
 
 const greeter = (str) => `Hello, ${str}!`;
 
@@ -7,6 +13,7 @@ console.log(greeter("Harry"));
 console.log("---------------------------------------");
 
 // What type? function
+// Write a function which returns the type of the argument we pass in.
 
 const checkType = (arg) => typeof arg;
 
@@ -20,6 +27,7 @@ console.log(
 console.log("---------------------------------------");
 
 // Check length function
+// Write a function which returns the length of the string we pass in.
 
 const checkLength = (string) => string.length;
 
